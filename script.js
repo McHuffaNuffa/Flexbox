155e388517b0593f12ab53ccff8f37a45a64f344
Jacob Harm <jacobharm@MacBookPro.attlocal.net>
2025-01-08 16:06:19 -0500

    Tried to implement modal

---
+++ script.js
@@ -0,0 +1,15 @@
+function flexStart() {
+const button = document.querySelectorAll("button")
+console.log("flex-start")
+}
+//For clicking on the ai pictures and making them pop up
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modal-content");
+const thumbnail = document.getElementsByClassName("thumbnail");
+function showModal() {
+  modal.style.display = "block";
+  modalContent.src = thumbnail.src;
+}
+function hideModal(){
+  modal.style.display = "none";
+}
